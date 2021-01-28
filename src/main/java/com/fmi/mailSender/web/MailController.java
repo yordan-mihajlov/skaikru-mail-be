e5b11987f8eba96d0aba60341f4e4a33093faab7
Yordan Mihajlov <yordan.mihajlov@gmail.com>
2021-01-28 10:26:35 +0200

    add cross origin import

--- src/main/java/com/fmi/mailSender/web/MailController.java
+++ src/main/java/com/fmi/mailSender/web/MailController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
